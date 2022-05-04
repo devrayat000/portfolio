bed3282ea24ea380cd8a87ee57eaf144e169568c
devrayat000 <2010050@me.buet.ac.bd>
2022-05-04 14:51:31 +0600

    added social icons in nav

--- src/lib/components/home/navbar.tsx
+++ src/lib/components/home/navbar.tsx
@@ -1,18 +1,52 @@
 import { pageFade } from '$lib/animation/fade'
-import { Avatar, Center, Navbar, Indicator } from '@mantine/core'
+import {
+  Avatar,
+  Divider,
+  Navbar,
+  Indicator,
+  Stack,
+  Title,
+  Text,
+  Group,
+  ActionIcon,
+  useMantineTheme,
+} from '@mantine/core'
 import { motion, useMotionValue, useTransform } from 'framer-motion'
+import {
+  BrandFacebook,
+  BrandLinkedin,
+  BrandTwitter,
+  BrandTelegram,
+  type Icon,
+} from 'tabler-icons-react'
+
+interface SocialIconProp {
+  icon: Icon
+  link?: string
+}
+
+const socialActions: SocialIconProp[] = [
+  {
+    icon: BrandFacebook,
+  },
+  {
+    icon: BrandLinkedin,
+  },
+  {
+    icon: BrandTwitter,
+  },
+  {
+    icon: BrandTelegram,
+  },
+]
 
 const MyNavbar = () => {
-  const opacity = useMotionValue(0)
-  const size = useTransform(opacity, [0, 1], [2, 20])
+  const theme = useMantineTheme()
   return (
     <Navbar width={{ base: 280 }} p="sm">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <Center>
+      {/* <Center> */}
+      <Stack align="center">
+        <motion.div variants={pageFade}>
           <Indicator
             inline
             size={20}
@@ -30,8 +64,29 @@ const MyNavbar = () => {
               //   withPlaceholder
             />
           </Indicator>
-        </Center>
-      </motion.div>
+        </motion.div>
+        <Title order={4}>Zul Ikram Musaddik Rayat</Title>
+        <Text variant="text" color="dimmed">
+          Full-Stack Developer
+        </Text>
+        <Group>
+          {socialActions.map((s, index) => (
+            <ActionIcon
+              key={index}
+              color="primaryColor"
+              size="lg"
+              radius="xl"
+              variant="filled"
+            >
+              <s.icon
+                color={theme.colorScheme === 'dark' ? theme.black : theme.white}
+              />
+            </ActionIcon>
+          ))}
+        </Group>
+        <Divider my="sm" size="sm" style={{ alignSelf: 'stretch' }} />
+      </Stack>
+      {/* </Center> */}
     </Navbar>
   )
 }
